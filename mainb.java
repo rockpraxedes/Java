d9dfeacb86f23047739e856eb9be1dc2b1b1f0f4
Sérgio Praxedes <rock.praxedes@yahoo.com.br>
2015-09-01 15:48:48 -0300

    Create mainb.java

---
+++ mainb.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.io.*;
+
+
+
+public class mainb{	
+
+	public static void main(String[] args) {
+		
+		Tela Tango = new Tela ();
+	
+
+	}
+}	
